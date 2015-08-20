fbf7e5d1eb6e5af6e865560e6dd9c69cdacff9cf
Alexandre <xan.marconi@hotmail.com>
2015-08-20 15:13:37 -0300

    update for multiples news

---
+++ SportsNews/TestHTMLReader.swift
@@ -0,0 +1,93 @@
+//
+//  SportHTLMReader.swift
+//  SportsNews
+//
+//  Created by Alexandre Calil Marconi on 8/19/15.
+//  Copyright (c) 2015 Alexandre Calil Marconi. All rights reserved.
+//
+
+import Foundation
+import HTMLReader
+import Alamofire
+import Result
+
+
+class TesteHTMLReader {
+    
+    var news = [News]()
+    var i = 0
+    
+    private lazy var manager: Alamofire.Manager = {
+        
+        let configuration: NSURLSessionConfiguration = {
+            var configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+            
+            var headers = Alamofire.Manager.defaultHTTPHeaders
+            headers["Accept-Encoding"] = "gzip"
+            headers["Content-Type"] = "text/html; charset=utf-8"
+            configuration.HTTPAdditionalHeaders = headers
+            
+            return configuration
+            }()
+        
+        return Manager(configuration: configuration)
+        }()
+    
+    func getNewsFromURL(URL: NSURL, completion: ((Result<[News], NSError?>)  -> Void)){
+        
+        manager.request(.GET, URL).responseString { (_, _, responseData, error) in
+            
+            if let stringData = responseData {
+                
+                let filtered = stringData.stringByReplacingOccurrencesOfString("<![CDATA[", withString: "").stringByReplacingOccurrencesOfString("]]>", withString: "")
+                let document = HTMLDocument(string: filtered)
+                
+                let divHeadings = document.nodesMatchingSelector("item") as! [HTMLElement]
+                
+                for div in divHeadings {
+                    
+                    var currentNews = News()
+                    
+                    for htmlNode in div.childElementNodes {
+                        //println("\nHTMLContent: \(htmlNode.textContent) - HTMLnode: \(htmlNode)\n")
+                        if let element = htmlNode as? HTMLElement {
+                            
+                            if let e = element.firstNodeMatchingSelector("title") {
+                                //println("title - \(e.textContent)")
+                                currentNews.title = e.textContent
+                            }
+                            
+                            if let e = element.firstNodeMatchingSelector("img"), imgPath = e.attributes["src"] as? String{
+                                println("imgPath - \(imgPath)")
+                                self.i++
+                                println(self.i)
+                                currentNews.imageURL = e.textContent
+                            }
+                            
+                            if let e = element.firstNodeMatchingSelector("description") {
+                                //println("description - \(e.textContent)")
+                                currentNews.text = e.textContent
+                            }
+                            
+                            if let e = element.firstNodeMatchingSelector("guid") {
+                                //println("link -\(e.textContent)")
+                                currentNews.link = e.textContent
+                            }
+                            
+                        }
+                    }
+                    self.news.append(currentNews)
+                }
+                
+                completion(Result.success(self.news))
+                
+            }
+            else {
+                completion(Result.failure(error))
+                println("error parsing responseData")
+            }
+        }
+        
+        
+    }
+}
--- SportsNews/ViewController.swift
+++ SportsNews/ViewController.swift
@@ -11,8 +11,9 @@ import Result
 
 class ViewController: UIViewController {
     
-    var news = [News]()
     let sportHTMLReader = SportHTLMReader()
+    let testHTMLReader = TesteHTMLReader()
+    var i = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +26,36 @@ class ViewController: UIViewController {
 //        println("Titulo - \(news.first?.text)")
         
 
+//        let url = NSURL(string: "http://www.lancenet.com.br/rss/section/3/")!
+//        sportHTMLReader.getNewsFromURL(url) { (result: Result<[News], NSError?>) -> Void in
+//            
+//            println("Finding news...")
+//            
+//            if let news = result.value {
+//                for currentNews in news{
+//                    println("Titulo da noticia - \(currentNews.title)")
+//                }
+//            }
+//            else{
+//                println("Error for getting news= \(result.error)")
+//            }
+//        }
+        
         let url = NSURL(string: "http://www.lancenet.com.br/rss/section/3/")!
-        sportHTMLReader.getNewsFromURL(url) { (result: Result<[News], NSError?>) -> Void in
+        testHTMLReader.getNewsFromURL(url) { (result: Result<[News], NSError?>) -> Void in
             
-            println("Finding news...")
-            
-            if let news = result.value {
-                for currentNews in news{
-                    println("Titulo da noticia - \(currentNews.title)")
+            if let news = result.value{
+                for n in news {
+                    self.i++
+                    println(self.i)
+                    println("\n")
+                    println("\n titulo - \(n.title)")
+                    println("\n image - \(n.imageURL)")
+                    println("\n texto - \(n.text)")
+                    println("\n link -\(n.link)")
+                    println("\n--------------------------------------------")
+                    
                 }
-            }
-            else{
-                println("Error for getting news= \(result.error)")
             }
         }
     }
